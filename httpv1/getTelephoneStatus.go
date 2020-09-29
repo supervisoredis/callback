e490539f411a68e0e82d7c1e21e5b02a50115b93
zhengyin <Zy211985@sjtu.edu.cn>
2020-09-29 16:15:36 +0800

    version 1.0

--- httpv1/getTelephoneStatus.go
+++ httpv1/getTelephoneStatus.go
@@ -6,6 +6,8 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"net/url"
+	"strings"
 	"time"
 )
 
@@ -17,9 +19,26 @@ func Telephonestatus(_ http.ResponseWriter, req *http.Request) {
 	status := string(body)
 	_ = json.Unmarshal([]byte(status), &t)
 	//保存到日志中
-	l := "[TELEPHONE_STATUS]" + time.Now().Format("2006-01-02 15:04:05") + ":  status: " + t.Status + ",  message: " + t.Msg
-	module.WriteLog("telephone_log.log", l)
 	if t.Status == "0" || t.Status == "1" {
-
+		l := "[TELEPHONE_STATUS]" + time.Now().Format("2006-01-02 15:04:05") + ":  status: " + t.Status + ",  message: " + t.Msg
+		module.WriteLog("telephone_log.log", l)
+		urlValuse := url.Values{
+			"endpoint":   {t.Result.Endpoint},
+			"sname":      {t.Result.Sname},
+			"event_type": {t.Result.Event_type},
+			"phone":      {t.Result.Phone},
+		}
+		reqBody := urlValuse.Encode()
+		conf := module.C.GetConf()
+		resp, err := http.Post(conf.CallbackAddress, "application/json;charset=UTF-8", strings.NewReader(reqBody))
+		if err != nil {
+			module.WriteLog("ERROR.log", err.Error())
+		}
+		ll := "[INFO resend]" + time.Now().Format("2006-01-02 15:04:05") + ":  endpoint: " + t.Result.Endpoint + ",  sname: " + t.Result.Sname + ", event_type: " + t.Result.Event_type + ", phone: " + t.Result.Phone
+		module.WriteLog("alarm_log.log", ll)
+		defer resp.Body.Close()
+	} else {
+		l := "[TELEPHONE_STATUS]" + time.Now().Format("2006-01-02 15:04:05") + ":  status: " + t.Status + ",  message: " + t.Msg
+		module.WriteLog("telephone_log.log", l)
 	}
 }
