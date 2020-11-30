29d1c110abd92b220f716ff46b5e39bd32926b39
zhengyin <1239628095@qq.com>
2020-11-30 19:02:45 +0800

    添加了次数和时间限制

--- httpv1/getTelephoneStatus.go
+++ httpv1/getTelephoneStatus.go
@@ -12,31 +12,34 @@ import (
 	"time"
 )
 
-var t data.Telephonestatus
-
 //提供一个获取请求的接口，处理和记录返回的电话拨通的状态和信息
 func Telephonestatus(_ http.ResponseWriter, req *http.Request) {
 	body, _ := ioutil.ReadAll(req.Body)
 	status := string(body)
+	var t data.Telephonestatus
 	_ = json.Unmarshal([]byte(status), &t)
 	//保存到日志中
+	go reSend(t)
+}
+func reSend(t data.Telephonestatus) {
 	if t.Status == "1" && t.Alarm.Times != "5" {
-		time.Sleep(300 * time.Second)
 		times, _ := strconv.Atoi(t.Alarm.Times)
 		times = times + 1
-		l := "[TELEPHONE_STATUS]" + time.Now().Format("2006-01-02 15:04:05") + ":  status: " + t.Status + ",  message: " + t.Msg
-		module.WriteLog("telephone_log.log", l)
+		//l := "[TELEPHONE_STATUS]" + time.Now().Format("2006-01-02 15:04:05") + ":  status: " + t.Status + ",  message: " + t.Msg
 		reqBody := "endpoint=" + t.Alarm.Endpoint + "&\nsname=" + t.Alarm.Sname + "&\nevent_type=" + t.Alarm.Event_type + "&\nphone=" + t.Alarm.Phone + "&\ntimes=" + strconv.Itoa(times)
+		module.WriteLog("telephone_log.log", reqBody)
 		fmt.Println(reqBody)
 		conf := module.C.GetConf()
+		time.Sleep(300 * time.Second)
 		resp, err := http.Post(conf.CallbackAddress, "text/plain", strings.NewReader(reqBody))
 		if err != nil {
 			module.WriteLog("ERROR.log", err.Error())
 		}
-		ll := "[INFO resend]" + time.Now().Format("2006-01-02 15:04:05") + ":  endpoint: " + t.Alarm.Endpoint + ",  sname: " + t.Alarm.Sname + ", event_type: " + t.Alarm.Event_type + ", phone: " + t.Alarm.Phone
-		module.WriteLog("alarm_log.log", ll)
+		//ll := "[INFO resend]" + time.Now().Format("2006-01-02 15:04:05") + ":  endpoint: " + t.Alarm.Endpoint + ",  sname: " + t.Alarm.Sname + ", event_type: " + t.Alarm.Event_type + ", phone: " + t.Alarm.Phone
+		module.WriteLog("alarm_log.log", reqBody)
 		defer resp.Body.Close()
 	} else {
+		fmt.Println(t)
 		l := "[TELEPHONE_STATUS]" + time.Now().Format("2006-01-02 15:04:05") + ":  status: " + t.Status + ",  message: " + t.Msg
 		module.WriteLog("telephone_log.log", l)
 	}
--- httpv1/postAlarmInfo.go
+++ httpv1/postAlarmInfo.go
@@ -8,80 +8,72 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
-	"time"
 )
 
-var a data.Alarm
-
 //提供一个获取用户数据和告警信息的接口，通过比较和处理，拼接成为新的JSON格式数据使用POST发送给回调接口
-func PostAlarmInfo(w http.ResponseWriter, req *http.Request) {
+func PostAlarmInfo(_ http.ResponseWriter, req *http.Request) {
 	body, _ := ioutil.ReadAll(req.Body)
 	alarm := string(body)
-	//fmt.Println(alarm)
+	//fmt.Println("最开始接受到的信息是：" + alarm)
+	var a data.Alarm
 	json.Unmarshal([]byte(alarm), &a)
+	fmt.Println("最开始接受到的信息是：" + a.Endpoint)
 	data.GetAllUserInfo()
-	if a.Sname == "主机失联" {
-		if module.ServerPing(a.Endpoint) {
-			conf := module.C.GetConf()
-			for _, i := range a.Users {
-				for _, j := range data.U.Dat.List {
-					if i == j.Username {
-						reqBody := "endpoint=" + a.Endpoint + "&\nsname=agent断开连接&\nevent_type=" + a.Event_type + "&\nphone=" + j.Phone + "&\ntimes=1"
-						resp, err := http.Post(conf.CallbackAddress, "text/plain", strings.NewReader(reqBody))
-						l := "[INFO]" + time.Now().Format("2006-01-02 15:04:05") + ":  endpoint: " + a.Endpoint + ",  sname: agent断开连接, event_type:" + a.Event_type + "  phone:" + j.Phone
-						module.WriteLog("alarm_log.log", l)
-						if err != nil {
-							module.WriteLog("ERROR.log", err.Error())
-						}
-						defer resp.Body.Close()
-					}
+	for _, i := range a.Users {
+		for _, j := range data.U.Dat.List {
+			if i == j.Username {
+				fmt.Println("循环之后进入了：" + a.Endpoint)
+				reqBody := "endpoint=" + a.Endpoint + "&\nsname=" + a.Sname + "&\nevent_type=" + a.Event_type + "&\nphone=" + j.Phone + "&\ntimes=1"
+				//fmt.Println(alarm)
+				fmt.Println(reqBody)
+				conf := module.C.GetConf()
+				resp, err := http.Post(conf.CallbackAddress, "text/plain", strings.NewReader(reqBody))
+				//l := "[INFO]" + time.Now().Format("2006-01-02 15:04:05") + ":  endpoint: " + a.Endpoint + ",  sname: " + a.Sname + ", event_type: " + a.Event_type + ", phone: " + j.Phone
+				module.WriteLog("alarm_log.log", alarm)
+				if err != nil {
+					module.WriteLog("ERROR.log", err.Error())
 				}
-			}
-		} else {
-			conf := module.C.GetConf()
-			for _, i := range a.Users {
-				for _, j := range data.U.Dat.List {
-					if i == j.Username {
-						reqBody := "endpoint=" + a.Endpoint + "&\nsname=服务器断开连接&\nevent_type=" + a.Event_type + "&\nphone=" + j.Phone + "&\ntimes=1"
-						resp, err := http.Post(conf.CallbackAddress, "text/plain", strings.NewReader(reqBody))
-						l := "[INFO]" + time.Now().Format("2006-01-02 15:04:05") + ":  endpoint: " + a.Endpoint + ",  sname: 服务器断开连接, event_type:" + a.Event_type + "   phone:" + j.Phone
-						module.WriteLog("alarm_log.log", l)
-						if err != nil {
-							module.WriteLog("ERROR.log", err.Error())
-						}
-						defer resp.Body.Close()
-					}
-				}
-			}
-			////fmt.Println("ping fail")
-			//conf := module.C.GetConf()
-			//reqBody := "endpoint=" + a.Endpoint + "服务器&\nsname=主机断开连接，请及时关注&\nevent_type=\nphone=" + conf.AdminPhone
-			//
-			//resp, err := http.Post(conf.CallbackAddress, "text/plain", strings.NewReader(reqBody))
-			//l := "[INFO]" + time.Now().Format("2006-01-02 15:04:05") + ":  endpoint: " + a.Endpoint + ",  sname: 主机断开连接, event_type:alert , phone:15601740807 "
-			//module.WriteLog("alarm_log.log", l)
-			//if err != nil {
-			//	module.WriteLog("ERROR.log", err.Error())
-			//}
-			//defer resp.Body.Close()
-		}
-	} else {
-		for _, i := range a.Users {
-			for _, j := range data.U.Dat.List {
-				if i == j.Username {
-					reqBody := "endpoint=" + a.Endpoint + "&\nsname=" + a.Sname + "&\nevent_type=" + a.Event_type + "&\nphone=" + j.Phone + "&\ntimes=1"
-					fmt.Println(reqBody)
-					conf := module.C.GetConf()
-					resp, err := http.Post(conf.CallbackAddress, "text/plain", strings.NewReader(reqBody))
-					l := "[INFO]" + time.Now().Format("2006-01-02 15:04:05") + ":  endpoint: " + a.Endpoint + ",  sname: " + a.Sname + ", event_type: " + a.Event_type + ", phone: " + j.Phone
-					module.WriteLog("alarm_log.log", l)
-					if err != nil {
-						module.WriteLog("ERROR.log", err.Error())
-					}
-					defer resp.Body.Close()
-				}
+				defer resp.Body.Close()
 			}
 		}
+		//if a.Sname == "主机失联" {
+		//	if module.ServerPing(a.Endpoint) {
+		//		conf := module.C.GetConf()
+		//		for _, i := range a.Users {
+		//			for _, j := range data.U.Dat.List {
+		//				if i == j.Username {
+		//					reqBody := "endpoint=" + a.Endpoint + "&\nsname=agent断开连接&\nevent_type=" + a.Event_type + "&\nphone=" + j.Phone + "&\ntimes=1"
+		//					resp, err := http.Post(conf.CallbackAddress, "text/plain", strings.NewReader(reqBody))
+		//					//l := "[INFO]" + time.Now().Format("2006-01-02 15:04:05") + ":  endpoint: " + a.Endpoint + ",  sname: agent断开连接, event_type:" + a.Event_type + "  phone:" + j.Phone
+		//					module.WriteLog("alarm_log.log", alarm)
+		//					if err != nil {
+		//						module.WriteLog("ERROR.log", err.Error())
+		//					}
+		//					defer resp.Body.Close()
+		//				}
+		//			}
+		//		}
+		//	} else {
+		//		conf := module.C.GetConf()
+		//		for _, i := range a.Users {
+		//			for _, j := range data.U.Dat.List {
+		//				if i == j.Username {
+		//					reqBody := "endpoint=" + a.Endpoint + "&\nsname=服务器断开连接&\nevent_type=" + a.Event_type + "&\nphone=" + j.Phone + "&\ntimes=1"
+		//					resp, err := http.Post(conf.CallbackAddress, "text/plain", strings.NewReader(reqBody))
+		//					//l := "[INFO]" + time.Now().Format("2006-01-02 15:04:05") + ":  endpoint: " + a.Endpoint + ",  sname: 服务器断开连接, event_type:" + a.Event_type + "   phone:" + j.Phone
+		//					module.WriteLog("alarm_log.log", alarm)
+		//					if err != nil {
+		//						module.WriteLog("ERROR.log", err.Error())
+		//					}
+		//					defer resp.Body.Close()
+		//				}
+		//			}
+		//		}
+		//	}
+		//} else {
+
+		//	}
+		//}
+		//从告警信息中读取用户名数据，对比获取到的用户数据，得到电话字段，与告警信息做拼接形成新的告警模式
 	}
-	//从告警信息中读取用户名数据，对比获取到的用户数据，得到电话字段，与告警信息做拼接形成新的告警模式
 }
